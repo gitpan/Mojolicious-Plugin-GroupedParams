d1f42df75d2d4a757f35a1dfdb818e3d31e011f1
Ivan Sokolov <ivsokolov@cpan.org>
2012-05-06 15:23:22 +0000

    Import of IVSOKOLOV/Mojolicious-Plugin-GroupedParams-0.04 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-GroupedParams
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         IVSOKOLOV/Mojolicious-Plugin-GroupedParams-0.04.tar.gz
    gitpan-cpan-author:       IVSOKOLOV
    gitpan-cpan-maturity:     released

--- lib/Mojolicious/Plugin/GroupedParams.pm
+++ lib/Mojolicious/Plugin/GroupedParams.pm
@@ -5,10 +5,14 @@ use strict;
 
 use base 'Mojolicious::Plugin';
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 sub register {
-    my ( $self, $app ) = @_;
+    my ( $self, $app, $conf ) = @_;
+
+    my $dms = $conf->{delimiters} || ['.'];
+    my $dms_re = join( '|', map { quotemeta($_) } @$dms );
+    my $key_split_re = qr/^(.+?)(?:$dms_re)(.+)$/;
 
     $app->helper(
         grouped_params => sub {
@@ -20,14 +24,14 @@ sub register {
                 my $params = $self->req->params->to_hash;
 
                 for my $key ( keys %$params ) {
-                   if ( my ($group, $name) = $key =~ /^([^.]+)\.(.+)$/ ) {
+                   if ( my ($group, $name) = $key =~ $key_split_re ) {
                        $groups->{$group} ||= {};
                        $groups->{$group}{$name} = $params->{$key};
                    }
                 }
 
-            } 
-            else { 
+            }
+            else {
                 $groups = $self->stash('grouped_params');
             }
 
@@ -46,7 +50,7 @@ Mojolicious::Plugin::GroupedParams - grouped params from query.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.04
 
 =head1 SYNOPSIS
 
@@ -60,15 +64,23 @@ Version 0.01
         my $new_article = $self->grouped_params('article');
 
         $self->db->resultset('Article')->create($new_article);
-        
+
     };
 
     # In template
     <input name="article.name" value="<%= grouped_params('article')->{name} %>" />
     <textarea name="article.text"><%= grouped_params('article')->{text} %></textarea>
-    
 
-=head1 FUNCTIONS 
+
+=head1 CONFIG
+
+=head2 delimiters
+
+Set delimiters to split parameters names. Default is [ "." ]
+
+    $self->plugin( 'grouped_params', {delimiter => [ "-", "."] } )
+
+=head1 FUNCTIONS
 
 =head2 register
 
@@ -76,7 +88,7 @@ Register plugin
 
 =head2 grouped_params
 
-This helper groups params with name like <group>.<name> 
+This helper groups params with name like <group>.<name>
 and put grouped params into stash variable "grouped_params".
 
 =head1 AUTHOR
